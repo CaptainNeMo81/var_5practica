4507dcce1b0b2bd581fe08d8486abac82ab518a1
Ne_Mo <captainne_mo@mail.ru>
2025-05-16 16:04:05 +0000

    Добавлен app.py второй версии

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-# app.py
 from flask import Flask, jsonify
 import time
 
 app = Flask(__name__)
+time_requests = 0
 
 @app.route('/time')
 def get_time():
+    global time_requests
+    time_requests += 1
     return jsonify({"time": int(time.time())})
+
+@app.route('/metrics')
+def get_metrics():
+    return jsonify({"count": time_requests})
